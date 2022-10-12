abfb35e701cd3870428d68a76c95e2276a9d1078
tanveerisonline <tanveerisonline@gmail.com>
2022-10-12 18:41:14 +0530

    ionic web camera app uploader to git

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
-
-const container = document.getElementById('root');
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
+defineCustomElements(window);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,36 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonImg,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+//import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+import { Camera, CameraResultType } from "@capacitor/camera";
+import { useState } from "react";
 
 const Home: React.FC = () => {
+  const [image, setImage] = useState<any>("");
+  const takePicture = async () => {
+    const cameraResult = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    setImage(cameraResult.webPath);
+
+    // Can be set to the src of an image now
+    //imageElement.src = imageUrl;
+    console.log(image);
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -10,13 +38,10 @@ const Home: React.FC = () => {
           <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen className="ion-padding">
+        <div>Basic Camera App</div>
+        <IonButton onClick={() => takePicture()}>Click Me</IonButton>
+        <IonImg src={image}></IonImg>
       </IonContent>
     </IonPage>
   );
